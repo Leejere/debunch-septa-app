2eec04915925b8877a276059feb60ce0e1812b6d
Leejere <lijfujian@gmail.com>
2023-04-10 14:33:08 -0400

    figured file structure

---
+++ src/components/other-components/Nav.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import septaLogo from "../../assets/septa-logo.png";
+
+export default function Nav() {
+  return (
+    <>
+      <p>Hello</p>
+      <img src={septaLogo}></img>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import "./css/styles.scss";
+import "./css/customized-bootstrap.scss";
 import { createRoot } from "react-dom/client";
 import Hello from "./components/Hello";
+import Nav from "./components/other-components/Nav";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<Hello />);
+root.render(<Nav />);
