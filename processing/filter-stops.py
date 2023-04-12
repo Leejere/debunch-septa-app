bde3fe135aa5b7b3337380972bf952c0f7aed0e5
Leejere <lijfujian@gmail.com>
2023-04-11 22:52:15 -0400

    added stops data

---
+++ processing/filter-stops.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import geopandas as gpd
+
+stops = gpd.read_file("db/stops-all.geojson")
+
+selected_routes = ["47", "21", "33", "7"]
+stops_selected = stops.query("LineAbbr in @selected_routes").copy()
+
+stops_selected.to_file("db/stops_selected.geojson", driver="GeoJSON")
