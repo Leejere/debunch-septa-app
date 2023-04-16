376eec8c455ef17b3d376407f6cf3781a9e2c265
Leejere <lijfujian@gmail.com>
2023-04-16 15:14:29 -0400

    able to show active trips/buses

--- src/components/panel/BusTripSelector.jsx
+++ src/components/panel/BusTripSelector.jsx
@@ -1,22 +1,53 @@
 import React, { useState, useEffect } from "react";
 import panelStyles from "./Panel.module.scss";
 import ModuleTitle from "./ModuleTitle";
+import capitalizeFirstLetter from "../../utils/capitalizeFirstLetter";
+import { directionDictReversed } from "../..";
+
+const TripList = React.memo(function ({ activeTrips }) {
+  const tripsEl = activeTrips.map((trip) => {
+    return (
+      <li key={trip.trip}>
+        <span>{trip.trip}</span>
+        <span>{trip.next_stop_name}</span>
+      </li>
+    );
+  });
+  return <ul>{tripsEl}</ul>;
+});
 
 export default React.memo(function ({
   requestParams,
   setRequestParams,
   realtimeData,
 }) {
-  console.log(realtimeData.bus);
+  const [activeTrips, setActiveTrips] = useState([]);
+  useEffect(() => {
+    if (!realtimeData) return;
+    const trips = realtimeData.bus.filter((bus) => {
+      return (
+        directionDictReversed[requestParams.route][
+          capitalizeFirstLetter(bus.Direction)
+        ] === requestParams.direction
+      );
+    });
+    setActiveTrips(trips);
+  }, [realtimeData]);
+
+  useEffect(() => {
+    console.log(activeTrips);
+  }, [activeTrips]);
+
+  const modalContent =
+    "Select a trip to predict. You can do so by clicking on this panel or on the map";
   return (
     <div className={panelStyles.module}>
       <ModuleTitle
         title={"Predicting for:"}
         modalHeading={"Trip Selection"}
-        modalContent={
-          "Select a trip to predict. You can do so by clicking on this panel or on the map"
-        }
+        modalContent={modalContent}
       />
+      <TripList activeTrips={activeTrips} />
     </div>
   );
 });
