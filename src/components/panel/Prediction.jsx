3cad0ac403b32bf90cdb8939aae7c0ff25cd29e6
Leejere <lijfujian@gmail.com>
2023-04-16 17:56:20 -0400

    fixed layout

--- src/components/panel/Prediction.jsx
+++ src/components/panel/Prediction.jsx
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from "react";
 import panelStyles from "./Panel.module.scss";
 import ModuleTitle from "./ModuleTitle";
+import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/Button";
+
+const PredictionList = React.memo(function ({ stopsSequence }) {
+  const predictionListEl = stopsSequence.map((stop, index) => {
+    return <ListGroup.Item key={index}>{stop.name}</ListGroup.Item>;
+  });
+  return <ListGroup>{predictionListEl}</ListGroup>;
+});
 
 export default React.memo(function ({
   stopsArray,
@@ -12,7 +21,10 @@ export default React.memo(function ({
   useEffect(() => {
     stopsArray.forEach((stop, index) => {
       if (Number(stop.id) === Number(currentStop)) {
+        const sliced = stopsArray.slice(index, index + 20);
+        console.log(sliced);
         setStopsSequence(stopsArray.slice(index, index + 20));
+        return;
       }
     });
   }, [currentStop]);
@@ -28,6 +40,7 @@ export default React.memo(function ({
         modalHeading={"Prediction Results"}
         modalContent={modalContent}
       />
+      <PredictionList stopsSequence={stopsSequence} />
     </div>
   );
 });
