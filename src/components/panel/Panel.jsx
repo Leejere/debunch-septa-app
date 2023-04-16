7b11b9ee7fa842f9866f4ca6b4a8a4492e5d7940
Leejere <lijfujian@gmail.com>
2023-04-16 00:39:44 -0400

    restructure

--- src/components/panel/Panel.jsx
+++ src/components/panel/Panel.jsx
@@ -1,39 +1,20 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
 import panelStyles from "./Panel.module.scss";
 
-import { directionDict } from "../../index.js";
+import RouteSelector from "./RouteSelector";
+import DirectionSelector from "./DirectionSelector";
 
-import { ButtonGroup, RouteSelector } from "./RouteSelector";
 export default function Panel({ requestParams, setRequestParams }) {
-  const directionOptions = {};
-  for (const key in directionDict) {
-    if (directionDict.hasOwnProperty(key)) {
-      const directions = directionDict[key];
-      directionOptions[key] = [directions[0], directions[1]];
-    }
-  }
-
-  const routeSelectButtonGroup = (
-    <RouteSelector
-      requestParams={requestParams}
-      setRequestParams={setRequestParams}
-    />
-  );
-  const directionSelectButtonGroup = (
-    <ButtonGroup
-      valueOptions={["0", "1"]}
-      displayOptions={directionOptions[requestParams.route]}
-      selected={requestParams.direction}
-      handleClick={(value) => {
-        setRequestParams({ ...requestParams, direction: value });
-      }}
-    ></ButtonGroup>
-  );
   return (
     <section className={panelStyles.container}>
-      {routeSelectButtonGroup}
-      {directionSelectButtonGroup}
+      <RouteSelector
+        requestParams={requestParams}
+        setRequestParams={setRequestParams}
+      />
+      <DirectionSelector
+        requestParams={requestParams}
+        setRequestParams={setRequestParams}
+      />
     </section>
   );
 }
