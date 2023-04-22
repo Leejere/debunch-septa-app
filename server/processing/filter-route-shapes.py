099117c36e9f9a9fc421f96d3d114a534205db2c
Leejere <lijfujian@gmail.com>
2023-04-22 15:21:19 -0400

    removed bus 7 using script

--- server/processing/filter-route-shapes.py
+++ server/processing/filter-route-shapes.py
@@ -5,7 +5,7 @@ import numpy as np
 url = "https://arcgis.dvrpc.org/portal/rest/services/Transportation/SEPTA_TransitRoutes/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"
 routes = gpd.read_file(url)
 
-selected_routes = ["47", "21", "33", "7"]
+selected_routes = ["47", "21", "33"]
 routes_selected = routes.query("lineabbr in @selected_routes").copy()
 
 routes_selected.to_file("db/routes_selected.geojson", driver="GeoJSON")
--- server/processing/filter-stops.py
+++ server/processing/filter-stops.py
@@ -4,7 +4,7 @@ import geopandas as gpd
 
 stops = gpd.read_file("db/stops-all.geojson")
 
-selected_routes = ["47", "21", "33", "7"]
+selected_routes = ["47", "21", "33"]
 stops_selected = stops.query("LineAbbr in @selected_routes").copy()
 
 stops_selected.to_file("db/stops_selected.geojson", driver="GeoJSON")
