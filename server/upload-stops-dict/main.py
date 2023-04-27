e6ea4b5f2c19281a78a021cc22121907b0465351
Leejere <li.jie.urban@gmail.com>
2023-04-27 15:49:52 -0400

    amended code to upload stopwise dictionary
    used json format

--- server/upload-stops-dict/main.py
+++ server/upload-stops-dict/main.py
@@ -1,27 +1,30 @@
 import pandas as pd
 import geopandas as gpd
 import dotenv
+import json
 from os.path import dirname
 from google.cloud import storage
 
 dotenv.load_dotenv()
+
+"""
+This script uploads two dictionary to Google Cloud Storage:
+1. Info by stopId
+2. Stop IDs of next 1-20 stops of each stopId
+"""
 
 script_dir = dirname(__file__)
 server_dir = dirname(script_dir)
 
 client = storage.Client()
 bucket = client.bucket("stop-dictionary")
-blob = bucket.blob("stop-info.csv")
+info_blob = bucket.blob("stop-info.json")
+next_stops_blob = bucket.blob("next-stops.json")
 
 runtime = pd.read_parquet(f"{server_dir}/raw-data/runtimeDf.gzip")
-stops = gpd.read_file(f"{server_dir}/data/stops/stopsGeographyProcessed.shp")
-
-stops = stops.rename(columns={"directionI": "directionId", "StopId": "stopId"}).drop(
-    "geography", axis=1
-)
 
 routes = ["21", "33", "47"]
-extract = (
+stop_info = (
     runtime.query("routeId.isin(@routes)")
     .copy()
     .groupby(["routeId", "directionId", "toStopId"])
@@ -31,11 +34,19 @@ extract = (
     .reset_index()
     .drop(["DoW"], axis=1)
 )
+
+stops = gpd.read_file(f"{server_dir}/data/stops/stopsGeographyProcessed.shp")
+stops = stops.rename(columns={"directionI": "directionId", "StopId": "stopId"}).drop(
+    "geography", axis=1
+)
+
 stops.routeId = stops.routeId.astype(str)
 stops.directionId = stops.directionId.astype(str)
 stops.stopId = stops.stopId.astype(str)
 
-extract = extract.merge(
+stops = stops.drop_duplicates(subset=["routeId", "directionId", "stopId"])
+# Info by Stop
+stop_info = stop_info.merge(
     stops[["routeId", "directionId", "stopId", "centerCity"]].rename(
         columns={"stopId": "toStopId"}
     ),
@@ -43,5 +54,54 @@ extract = extract.merge(
     on=["routeId", "directionId", "toStopId"],
 )
 
-csv = extract.to_csv(index=False)
-blob.upload_from_string(csv, content_type="text/csv")
+stop_info["stop_unique_id"] = (
+    stop_info["routeId"] + "_" + stop_info["directionId"] + "_" + stop_info["toStopId"]
+)
+
+
+# Next 1-20 stops by stop
+sorted = stop_info.sort_values(
+    [
+        "routeId",
+        "directionId",
+        "toStopPathIndex",
+    ]
+)
+
+for i in range(1, 22):
+    sorted[f"next_{i}_unique_id"] = sorted.groupby(["routeId", "directionId"])[
+        "stop_unique_id"
+    ].shift(-i)
+
+next_stops = sorted.drop(
+    [
+        "toStopPathIndex",
+        "routeId",
+        "directionId",
+        "toStopId",
+        "expectedHeadway",
+        "centerCity",
+    ],
+    axis=1,
+)
+
+stop_info = stop_info.drop(["routeId", "directionId", "toStopId"], axis=1)
+stop_info = stop_info.convert_dtypes()
+
+stop_info_dict = stop_info.set_index("stop_unique_id")[
+    [
+        "expectedHeadway",
+        "centerCity",
+    ]
+].to_dict(orient="index")
+
+next_stops = next_stops.convert_dtypes()
+next_stops_dict = next_stops.set_index("stop_unique_id").to_dict(orient="index")
+
+
+info_blob.upload_from_string(
+    json.dumps(stop_info_dict), content_type="application/json"
+)
+next_stops_blob.upload_from_string(
+    json.dumps(next_stops_dict), content_type="application/json"
+)
