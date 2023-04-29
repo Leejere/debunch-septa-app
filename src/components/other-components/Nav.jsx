67f5af61c6b8df3909ee36f587249f763be9eb24
Leejere <li.jie.urban@gmail.com>
2023-04-29 11:20:27 -0400

    changed modal styles

--- src/components/other-components/Nav.jsx
+++ src/components/other-components/Nav.jsx
@@ -9,13 +9,24 @@ import navStyles from "./Nav.module.scss";
 const ModalText = () => {
   return (
     <>
-      <h5 className="modalSubtitle">What is the project?</h5>
+      <h5 className="modalSubtitle">Frequency is freedom</h5>
+      <p>
+        Knowing when buses are coming, riders plan their trips without stress.
+        But disruptions occur and affect some buses more than others, leading to{" "}
+        <strong>bunching</strong>, where two buses arrive at a stop
+        consecutively, accompanied by
+        <strong>gapping</strong>, where riders have to wait a long time after
+        the last bus left. This should not happen too often to a reliable bus
+        service.
+      </p>
+      <h5 className="modalSubtitle">What this is</h5>
+      <p></p>
     </>
   );
 };
 
 export default function Nav() {
-  const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(true);
   const handleClose = () => setShowModal(false);
   const handleShow = () => setShowModal(true);
 
@@ -32,7 +43,12 @@ export default function Nav() {
           info
         </span>
       </div>
-      <Modal show={showModal} onHide={handleClose} size="lg">
+      <Modal
+        show={showModal}
+        onHide={handleClose}
+        size="lg"
+        dialogClassName={navStyles.wideModal}
+      >
         <Modal.Header closeButton>
           <Modal.Title className="modalTitle">SEPTA, Debunched</Modal.Title>
         </Modal.Header>
@@ -41,7 +57,7 @@ export default function Nav() {
         </Modal.Body>
         <Modal.Footer>
           <Button variant="primary" onClick={handleClose}>
-            Close
+            Explore
           </Button>
         </Modal.Footer>
       </Modal>
